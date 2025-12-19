f7556d0f2749b39fe75c866f1daf343fbc4e476b
gio <mail@example.com>
2025-12-19 22:53:29 +0400

    fix

--- store-app/apply-config.js
+++ store-app/apply-config.js
@@ -3,6 +3,6 @@ const fs = require('fs');
 fs.writeFileSync(
   'dist/store-app/browser/config.json',
   JSON.stringify({
-    apiUrl: process.env.API_BASE_URL,
+    apiBaseUrl: process.env.API_BASE_URL,
   }, null, 2)
 );
